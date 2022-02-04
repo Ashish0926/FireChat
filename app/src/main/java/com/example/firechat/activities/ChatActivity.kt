f6cf18ab619246d0763fb17961bb31270c18f1af
Ashish Gupta <ashishg0926@gmail.com>
2022-02-05 02:22:24 +0530

    added chatActivity

---
+++ app/src/main/java/com/example/firechat/activities/ChatActivity.kt
@@ -0,0 +1,15 @@
+package com.example.firechat.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firechat.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
